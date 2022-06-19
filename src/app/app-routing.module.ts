6c3bbbc3c22a3c319e687e4733598c076154215e
SergBobyr <serg_bobyr@ukr.net>
2022-06-19 13:10:14 +0300

    Модуль панели администратора

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
-import { NgModule } from '@angular/core';
-import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
-import {HomePageComponent} from "./home-page/home-page.component";
-import {PostPageComponent} from "./post-page/post-page.component";
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {PostPageComponent} from './post-page/post-page.component';
+
 
 const routes: Routes = [
-  {path:'', redirectTo:'/', pathMatch: 'full'},
-  {path: '', component: MainLayoutComponent, children: [
-    {path: '', component: HomePageComponent},
-    {path: 'post/:id', component: PostPageComponent}
-  ]
-},
-  {path: 'admin', loadChildren: './admin/admin.module#AdminModule'
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '', component: HomePageComponent},
+      {path: 'post/:id', component: PostPageComponent}
+    ]
+  },
+  {
+     path: 'admin',
+    // loadChildren: './admin/admin.module#AdminModule'
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
   }
 ];
 
@@ -21,4 +26,5 @@ const routes: Routes = [
   })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
